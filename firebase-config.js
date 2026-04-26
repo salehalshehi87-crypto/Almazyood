8aff3a071872e6dd303d587a2c405fed2828448b
salehalshehi87-crypto <saleh.alshehi87@gmail.com>
2026-04-27 01:34:52 +0400

    Update databaseURL in firebase-config.js
    Added trailing slash to databaseURL in Firebase config.

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyD6fmu_R_jd2i0shZeeGDe1UrcpDaWysbU",
   authDomain: "mazyoud-band.firebaseapp.com",
-  databaseURL: "https://mazyoud-band-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://mazyoud-band-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "mazyoud-band",
   storageBucket: "mazyoud-band.firebasestorage.app",
   messagingSenderId: "498068741979",
